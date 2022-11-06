946b287d2c0d34d459cdfbab2a13b0e6cd3a98a3
Hakkadaikon <hakkadaikon@yahoo.co.jp>
2022-11-07 00:19:20 +0900

    Add json output mode.

--- logger/logger.go
+++ logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"time"
@@ -23,17 +24,35 @@ const (
 	ErrorLogFileOpenFailed
 	ErrorLogFileCloseFailed
 	ErrorLevelNotEnough
+	ErrorJsonConvertFailed
 )
 
-type Logger struct {
-	level LoggerLevel
-	path  string
-	fp    *os.File
+type LoggerOutputType int
+
+const (
+	OutputTypeNormal LoggerOutputType = iota
+	OutputTypeJson
+)
+
+type LoggerJson struct {
+	Level   string `json:"level"`
+	Message string `json:"message"`
+	Date    string `json:"date"`
 }
 
-func (logger *Logger) Init(level LoggerLevel, path string) LoggerError {
+type Logger struct {
+	level   LoggerLevel
+	logtype LoggerOutputType
+	path    string
+	fp      *os.File
+}
+
+func (logger *Logger) Init(level LoggerLevel, path string, logtype LoggerOutputType) LoggerError {
 	if level > LevelError {
-		//fmt.Printf("Invalid argument. level[%d]\n", level)
+		return ErrorInvalidArgument
+	}
+
+	if logtype > OutputTypeJson {
 		return ErrorInvalidArgument
 	}
 
@@ -45,12 +64,12 @@ func (logger *Logger) Init(level LoggerLevel, path string) LoggerError {
 	var err error
 	logger.fp, err = os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		//fmt.Printf("open error. reason[%v]\n", err)
 		return ErrorLogFileOpenFailed
 	}
 
 	logger.level = level
 	logger.path = path
+	logger.logtype = logtype
 	return ErrorNone
 }
 
@@ -63,14 +82,13 @@ func (logger *Logger) Deinit() LoggerError {
 	return ErrorNone
 }
 
-func GetDateStr() string {
+func getDateStr() string {
 	now := time.Now()
 	str := fmt.Sprintf(
-		"%04d/%02d/%02d(%s) %02d:%02d:%02d",
+		"%04d/%02d/%02d %02d:%02d:%02d",
 		now.Year(),
 		now.Month(),
 		now.Day(),
-		now.Weekday(),
 		now.Hour(),
 		now.Minute(),
 		now.Second())
@@ -78,7 +96,7 @@ func GetDateStr() string {
 	return str
 }
 
-func GetLevelStr(level LoggerLevel) string {
+func getLevelStr(level LoggerLevel) string {
 	lvstr := "???"
 	switch level {
 	case LevelDebug:
@@ -94,30 +112,44 @@ func GetLevelStr(level LoggerLevel) string {
 	return lvstr
 }
 
-func (logger *Logger) Print(level LoggerLevel, str string) LoggerError {
+func (logger *Logger) print(level LoggerLevel, str string) LoggerError {
 	if level < logger.level {
 		return ErrorLevelNotEnough
 	}
 
-	lvstr := GetLevelStr(level)
-	now := GetDateStr()
-	fmt.Fprintf(logger.fp, "[%s] [%s] %s\n", now, lvstr, str)
+	lvstr := getLevelStr(level)
+	now := getDateStr()
+
+	switch logger.logtype {
+	case OutputTypeNormal:
+		fmt.Fprintf(logger.fp, "[%s] [%s] %s\n", now, lvstr, str)
+	case OutputTypeJson:
+		var tmpstruct LoggerJson
+		tmpstruct.Level = lvstr
+		tmpstruct.Date = now
+		tmpstruct.Message = str
+		json, err := json.Marshal(tmpstruct)
+		if err != nil {
+			return ErrorJsonConvertFailed
+		}
+		fmt.Fprintf(logger.fp, "%+v\n", string(json))
+	}
 
 	return ErrorNone
 }
 
 func (logger *Logger) Debug(str string) LoggerError {
-	return logger.Print(LevelDebug, str)
+	return logger.print(LevelDebug, str)
 }
 
 func (logger *Logger) Info(str string) LoggerError {
-	return logger.Print(LevelInfo, str)
+	return logger.print(LevelInfo, str)
 }
 
 func (logger *Logger) Warning(str string) LoggerError {
-	return logger.Print(LevelWarning, str)
+	return logger.print(LevelWarning, str)
 }
 
 func (logger *Logger) Error(str string) LoggerError {
-	return logger.Print(LevelError, str)
+	return logger.print(LevelError, str)
 }
--- main_test.go
+++ main_test.go
@@ -8,14 +8,18 @@ import (
 
 func TestInitInvalidArgument(t *testing.T) {
 	var log logger.Logger
-	if err := log.Init(4, ""); err != logger.ErrorInvalidArgument {
+	if err := log.Init(4, "", logger.OutputTypeNormal); err != logger.ErrorInvalidArgument {
+		t.Errorf("Test case[Init() Invalid argument] err:%d want:%d", err, logger.ErrorInvalidArgument)
+	}
+
+	if err := log.Init(logger.LevelDebug, "", 2); err != logger.ErrorInvalidArgument {
 		t.Errorf("Test case[Init() Invalid argument] err:%d want:%d", err, logger.ErrorInvalidArgument)
 	}
 }
 
 func TestInitOpenError(t *testing.T) {
 	var log logger.Logger
-	if err := log.Init(logger.LevelDebug, "/log/debug.log"); err != logger.ErrorLogFileOpenFailed {
+	if err := log.Init(logger.LevelDebug, "/log/debug.log", logger.OutputTypeNormal); err != logger.ErrorLogFileOpenFailed {
 		t.Errorf("Test case[Init() Open Error] err:%d want:%d", err, logger.ErrorLogFileOpenFailed)
 	}
 }
@@ -24,7 +28,7 @@ func TestInitDefault(t *testing.T) {
 	os.Remove("./info.log")
 
 	var log logger.Logger
-	if err := log.Init(logger.LevelDebug, ""); err != logger.ErrorNone {
+	if err := log.Init(logger.LevelDebug, "", logger.OutputTypeNormal); err != logger.ErrorNone {
 		t.Errorf("Test case[Init() Default] err:%d want:%d", err, logger.ErrorNone)
 	}
 
@@ -40,7 +44,7 @@ func TestInitSpecify(t *testing.T) {
 	os.Remove("log/test.log")
 
 	var log logger.Logger
-	if err := log.Init(logger.LevelDebug, "log/test.log"); err != logger.ErrorNone {
+	if err := log.Init(logger.LevelDebug, "log/test.log", logger.OutputTypeNormal); err != logger.ErrorNone {
 		t.Errorf("Test case[Init() Specify] err:%d want:%d", err, logger.ErrorNone)
 	}
 
@@ -53,8 +57,10 @@ func TestInitSpecify(t *testing.T) {
 }
 
 func TestDebug(t *testing.T) {
+	os.Remove("log/debug.log")
+
 	var log logger.Logger
-	log.Init(logger.LevelDebug, "log/debug.log")
+	log.Init(logger.LevelDebug, "log/debug.log", logger.OutputTypeNormal)
 
 	if err := log.Debug("str"); err != logger.ErrorNone {
 		t.Errorf("Test case[Debug() Debug] err:%d want:%d", err, logger.ErrorNone)
@@ -76,9 +82,37 @@ func TestDebug(t *testing.T) {
 	os.Remove("log/debug.log")
 }
 
-func TestInfo(t *testing.T) {
+func TestDebugJson(t *testing.T) {
+	os.Remove("log/debug.log")
+
 	var log logger.Logger
-	log.Init(logger.LevelInfo, "log/info.log")
+	log.Init(logger.LevelDebug, "log/debug.log", logger.OutputTypeJson)
+
+	if err := log.Debug("str"); err != logger.ErrorNone {
+		t.Errorf("Test case[Debug() Type:Json Debug] err:%d want:%d", err, logger.ErrorNone)
+	}
+
+	if err := log.Info("str"); err != logger.ErrorNone {
+		t.Errorf("Test case[Debug() Type:Json Info] err:%d want:%d", err, logger.ErrorNone)
+	}
+
+	if err := log.Warning("str"); err != logger.ErrorNone {
+		t.Errorf("Test case[Debug() Type:Json Warning] err:%d want:%d", err, logger.ErrorNone)
+	}
+
+	if err := log.Error("str"); err != logger.ErrorNone {
+		t.Errorf("Test case[Debug() Type:Json Error] err:%d want:%d", err, logger.ErrorNone)
+	}
+
+	log.Deinit()
+	os.Remove("log/debug.log")
+}
+
+func TestInfo(t *testing.T) {
+	os.Remove("log/info.log")
+
+	var log logger.Logger
+	log.Init(logger.LevelInfo, "log/info.log", logger.OutputTypeNormal)
 
 	if err := log.Debug("str"); err != logger.ErrorLevelNotEnough {
 		t.Errorf("Test case[Info() Debug] err:%d want:%d", err, logger.ErrorLevelNotEnough)
@@ -101,8 +135,10 @@ func TestInfo(t *testing.T) {
 }
 
 func TestWarning(t *testing.T) {
+	os.Remove("log/warning.log")
+
 	var log logger.Logger
-	log.Init(logger.LevelWarning, "log/warning.log")
+	log.Init(logger.LevelWarning, "log/warning.log", logger.OutputTypeNormal)
 
 	if err := log.Debug("str"); err != logger.ErrorLevelNotEnough {
 		t.Errorf("Test case[Warning() Debug] err:%d want:%d", err, logger.ErrorLevelNotEnough)
@@ -125,8 +161,10 @@ func TestWarning(t *testing.T) {
 }
 
 func TestError(t *testing.T) {
+	os.Remove("log/error.log")
+
 	var log logger.Logger
-	log.Init(logger.LevelError, "log/error.log")
+	log.Init(logger.LevelError, "log/error.log", logger.OutputTypeNormal)
 
 	if err := log.Debug("str"); err != logger.ErrorLevelNotEnough {
 		t.Errorf("Test case[Error() Debug] err:%d want:%d", err, logger.ErrorLevelNotEnough)
@@ -150,7 +188,7 @@ func TestError(t *testing.T) {
 
 func TestDeinit(t *testing.T) {
 	var log logger.Logger
-	log.Init(logger.LevelError, "log/test.log")
+	log.Init(logger.LevelError, "log/test.log", logger.OutputTypeNormal)
 
 	if err := log.Deinit(); err != logger.ErrorNone {
 		t.Errorf("Test case[Deinit() normal] err:%d want:%d", err, logger.ErrorNone)
